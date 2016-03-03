cd3064d619a4789202ade5d057c444c2cfbc935f
Ricardo Kim <hexadecimal.exe@gmail.com>
2016-03-03 16:34:54 -0300

    Update ALADES.c

--- ALADES.c
+++ ALADES.c
@@ -4,9 +4,7 @@
 
 
 int h2m(int a){
-    int conv;
-    conv=a* 60;
-return conv;
+    return a*60;
 }
 
 int main(){
@@ -14,23 +12,24 @@ int main(){
     while(h1>0||h2>0||m1>0||m2>0){
         scanf("%d %d %d %d",&h1,&m1,&h2,&m2);
         if(h1!=0||h2!=0||m1!=0||m2!=0){
-            if(h1>0){
+            if(h1>0)
                 m1+=h2m(h1);
-            }
-            if(h2>0){
+            
+            if(h2>0)
                 m2+=h2m(h2);
-            }
-            if(h1==0){
+            
+            if(h1==0)
                 m1+=dia;
-            }
-            if(h2 == 0){
+            
+            if(h2 == 0)
                 m2+=dia;
-            }
+            
             if(m2>m1)
                 printf("%d\n",m2-m1);
-            if(m1>m2){
+                
+            if(m1>m2)
                 printf("%d\n",m2+dia-m1);
-            }
+            
         }
     }
 return 0;
