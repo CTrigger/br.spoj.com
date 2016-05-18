d387659ccbbc72445ba81f65437b76fbc17a7ccb
Ricardo Kim <hexadecimal.exe@gmail.com>
2016-05-18 10:34:53 -0300

    Update PRIMO.c

--- PRIMO.c
+++ PRIMO.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 long long entrada,j=1 ,divisor = 1,checker=2,resto,verdade = 1;
-void primos(){
-    while (verdade < 3 && j < checker){
+void primos()
+{
+    while (verdade < 3 && j < checker)
+    {
         resto = entrada%j;
         checker = entrada/j;
         j++;
-        if(resto == 0){
+        
+        if(resto == 0)
             verdade ++;
-        }
+            
     }
-        if(verdade == 2){
-        puts("sim");
+        if(verdade == 2)
+        {
+            puts("sim");
         }else{
-        puts("nao");
+            puts("nao");
         }
 }
 
